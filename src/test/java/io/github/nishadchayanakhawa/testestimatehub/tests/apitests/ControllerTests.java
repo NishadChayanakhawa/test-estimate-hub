c3a2fbec5e1e513c58fdbe469edcca6ad51bdc47
nisha <nisha@pune-desktop>
2023-11-21 19:22:54 +0530

    TEH-2 application configuration api completed

--- src/test/java/io/github/nishadchayanakhawa/testestimatehub/tests/apitests/ControllerTests.java
+++ src/test/java/io/github/nishadchayanakhawa/testestimatehub/tests/apitests/ControllerTests.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features = "src/test/resources/Features/00_ControllerTests.feature",
+		features = "src/test/resources/Features/ControllerTests.feature",
 		glue = "io.nishadc.automationtestingframework.testinginterface.restapi.stepdefinitions",
 		tags="not @disabled")
 public class ControllerTests extends AbstractTestNGCucumberTests{
